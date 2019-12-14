3851828451de3a1c5ad5608d0c4756d6d72c32b2
hilmykufa <43564211+hilmykufa@users.noreply.github.com>
2019-12-14 18:50:34 +0700

    Versi 0.0.5

--- Mulungo_startup/Mulungo/app/src/main/java/com/example/mulungo/HomeActivity.java
+++ Mulungo_startup/Mulungo/app/src/main/java/com/example/mulungo/HomeActivity.java
@@ -4,24 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class HomeActivity extends AppCompatActivity {
     private TextView name, email;
     private Button btn_logout;
-
+    SessionManager sessionManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        sessionManager = new SessionManager(this);
+        sessionManager.checkLogin();
+
         name=findViewById(R.id.name);
         email=findViewById(R.id.email);
-        Intent intent = getIntent();
-        String extraName = intent.getStringExtra("name");
-        String extraMail = intent.getStringExtra("email");
-        name.setText(extraName);
-        email.setText(extraMail);
+        HashMap<String, String> user = sessionManager.getUSerDetail();
+        String mName = user.get(SessionManager.NAME);
+        String mEmail = user.get(SessionManager.EMAIL);
+        name.setText(mName);
+        email.setText(mEmail);
+        //Intent intent = getIntent();
+        //String extraName = intent.getStringExtra("name");
+       // String extraMail = intent.getStringExtra("email");
+
+        //name.setText(extraName);
+       // email.setText(extraMail);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sessionManager.logout();
+            }
+        });
 
     }
 }
--- Mulungo_startup/Mulungo/app/src/main/java/com/example/mulungo/loginActivity.java
+++ Mulungo_startup/Mulungo/app/src/main/java/com/example/mulungo/loginActivity.java
@@ -32,11 +32,13 @@ public class loginActivity extends AppCompatActivity {
     private TextView link_regist;
     private ProgressBar loading;
     private static String URL_LOGIN="http://192.168.1.66/android_register_login/login.php";
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sessionManager = new SessionManager(this);
         loading=findViewById(R.id.loading);
         email=findViewById(R.id.Email);
         password=findViewById(R.id.Password);
@@ -71,6 +73,7 @@ public class loginActivity extends AppCompatActivity {
                                     JSONObject object = jsonArray.getJSONObject(i);
                                     String name = object.getString("name").trim();
                                     String email = object.getString("email").trim();
+                                    sessionManager.createSession(name,email);
                                     Intent intent = new Intent(loginActivity.this,HomeActivity.class);
                                     intent.putExtra("name", name);
                                     intent.putExtra("email", email);
